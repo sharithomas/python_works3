a915cfa19a8ca8ed7324820871573a2e5a12d30e
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-01 13:10:52 +0530

    Create init.py

---
+++ class_programs/init.py
@@ -0,0 +1,9 @@
+#2.create init function
+class Person:
+    def __init__(self,name,age):
+        self.name1=name
+        self.age1=age
+        
+p1=Person("john",30)
+print(p1.age1)
+print(p1.name1)
