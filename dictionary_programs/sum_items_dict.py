95f38eb009590d56bf74ed7d48290f74fc324634
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-01 14:12:19 +0530

    Create sum_items_dict.py

---
+++ dictionary_programs/sum_items_dict.py
@@ -0,0 +1,5 @@
+# Write a Python program to sum all the items in a dictionary.
+
+my_dict = {'data1':500,'data2':-54,'data3':37,'data4':17}
+print("sum of dctionarry values")
+print(sum(my_dict.values()))
