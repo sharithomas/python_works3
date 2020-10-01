61cf4e79b6e84cce6125cdb8647a81785967a469
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-01 14:13:40 +0530

    Create remove_key.py

---
+++ dictionary_programs/remove_key.py
@@ -0,0 +1,7 @@
+#  Write a Python program to remove a key from a dictionary
+
+my_dict = {'data1':2,'data2':3,'data3':5,'data4':4}
+print("original dictionary",my_dict)
+del my_dict['data2']
+print("dictionary after deleting a key")
+print(my_dict)
