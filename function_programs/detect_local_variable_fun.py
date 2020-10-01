41250f3dff802575cd74221595a69c7d79d73b8f
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-01 18:22:05 +0530

    Create detect_local_variable_fun.py

---
+++ function_programs/detect_local_variable_fun.py
@@ -0,0 +1,10 @@
+# Write a Python program to detect the number of local variables declared in a function
+
+def abc():
+    x = 1
+    y = 2
+    str1= "w3resource"
+    z=3.4
+    print("Python Exercises")
+
+print(abc.__code__.co_nlocals)
