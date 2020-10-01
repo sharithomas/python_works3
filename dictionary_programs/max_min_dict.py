5746373d2658b1ff1dbb0921a0d7de124fa4a58d
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-01 14:15:55 +0530

    Create max_min_dict.py

---
+++ dictionary_programs/max_min_dict.py
@@ -0,0 +1,7 @@
+# Write a Python program to get the maximum and minimum value in a dictionary
+
+dictionary=dict({1:3,6:8,3:6,7:23,5:9,2:67})
+max_value=max(dictionary.values())
+min_value=min(dictionary.values())
+print("maximum value=", max_value)
+print("minimum valuue=",min_value)
