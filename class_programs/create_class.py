3174d647c600cf6acc12679e1c7a69252fb97e51
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-01 12:43:31 +0530

    Create create_class.py

---
+++ class_programs/create_class.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 16:03:03 2020
+@author: I325377
+"""
+
+#Class programs
+#1. create a calss and object
+class Myclass:
+    x=5
+    
+p1=Myclass()
+print(p1.x)
