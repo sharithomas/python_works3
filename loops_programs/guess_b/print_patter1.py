3ba5f0e5eb5da8102eaf2c2e3546ce5934b48f21
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:10:13 +0530

    Create print_patter1.py

---
+++ loops_programs/guess_b/print_patter1.py
@@ -0,0 +1,20 @@
+# Write a Python program to construct the following pattern, using a nested for loop.
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+n=5
+for i in range(1,n):
+    for j in range(i):
+        print('*', end=" ")
+    print("\n")   
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print("\n")
