c8b6e9cd06e5441cbd5c4f8c269e5269e32b632d
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:45:47 +0530

    Create remove_item.py

---
+++ set_programs/remove_item.py
@@ -0,0 +1,7 @@
+# Write a Python program to remove item(s) from set
+
+my_set={"apple","orange","banana","plum"}
+my_set.remove("apple") #remove item apple
+my_set.pop() #remove random item 
+my_set.discard("banana") #delete item banana
+print(my_set)
