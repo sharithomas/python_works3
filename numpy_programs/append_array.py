c1d7b061f3f57ec95b8657ca28a69c21a47d3c5d
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:06:50 +0530

    Create append_array.py

---
+++ numpy_programs/append_array.py
@@ -0,0 +1,8 @@
+
+# Write a NumPy program to append values to the end of an array
+
+import numpy as np
+array=np.array([1,2,3,4])
+print("original array\n",array)
+array1=np.append(array,[5,6,7])
+print("array after appending with new values",array1)
