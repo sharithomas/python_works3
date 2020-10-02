3b55b107b77400f1e7a734a76fcd13c380352dea
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:51:56 +0530

    Create max_min_set.py

---
+++ set_programs/max_min_set.py
@@ -0,0 +1,5 @@
+# Write a Python program to find maximum and the minimum value in a set.
+
+set1 = set([10,34,10, 2, 43, 14, 5])
+print("maximum value in set:",max(set1))
+print("minimum value in set:",min(set1))
