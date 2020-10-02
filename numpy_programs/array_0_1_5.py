8a23ec01903acf1adf0e6313a6c6813f0f931ced
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:13:02 +0530

    Create array_0_1_5.py

---
+++ numpy_programs/array_0_1_5.py
@@ -0,0 +1,11 @@
+# Write a NumPy program to create an array of 10 zeros,10 ones, 10 fives.
+
+import numpy as np
+array1=np.zeros(10)
+print("array with 10 zeros",array1)
+array2=np.ones(10)
+print("array with 10 ones",array2)
+array3=np.full(10,5)
+print("array with 10 fives",array3)
+array4=np.ones(10)*5
+print("array with 10 fives",array4)
