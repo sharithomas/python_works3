efc1198de881b8932b931d63e3f4a781eb7792c4
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:47:53 +0530

    Create union_sets.py

---
+++ set_programs/union_sets.py
@@ -0,0 +1,6 @@
+# Write a Python program to create a union of sets
+
+my_set={"apple","orange","banana","plum"}
+my_set1={"banana","plum","kiwi"}
+set_intersection=my_set.union(my_set1)
+print(set_intersection)
