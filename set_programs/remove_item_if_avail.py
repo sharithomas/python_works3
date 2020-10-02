d11a29e75778b720cfe9ceebd2b8d9e38adc3ba8
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:46:49 +0530

    Create remove_item_if_avail.py

---
+++ set_programs/remove_item_if_avail.py
@@ -0,0 +1,6 @@
+#  Write a Python program to remove an item from a set if it is present in the set
+
+my_set={"apple","orange","banana","plum"}
+my_set.discard("grapes")
+my_set.discard("banana")
+print(my_set)
