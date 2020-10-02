486c6608bbe8eb34b0232ba17cc5dd1253cc8d97
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:06:14 +0530

    Create list_tuple_array.py

---
+++ numpy_programs/list_tuple_array.py
@@ -0,0 +1,7 @@
+# Write a NumPy program to convert a list and tuple into arrays.
+
+import numpy as np
+list1=[1,2,3,4,5,6,7]
+tuple1=([1,2,3],[2,3,4])
+print("list to array\n",np.array(list1))
+print("tuple to array\n",np.array(tuple1))
