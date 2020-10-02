64c2068ac80f287992ea87e1ca18e5d253962585
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:58:36 +0530

    Create unpack_tuple.py

---
+++ tuple_programs/unpack_tuple.py
@@ -0,0 +1,9 @@
+# Write a Python program to unpack a tuple in several variables
+
+tuple1=1,2,3,4
+w,x,y,z =tuple1 #unpacking tuple in variables
+print("sum=",w+x+y+z)
+print("w=",w)
+print("x=",x)
+print("y=",y)
+print("z=",z)
