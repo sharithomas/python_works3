c9d6a745d3220799ba9ce4eb88888551dbee04fd
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:48:25 +0530

    Create set_diff.py

---
+++ set_programs/set_diff.py
@@ -0,0 +1,8 @@
+# Write a Python program to create set difference.
+
+set1={"apple","orange","banana","plum"}
+set2={"banana","plum","kiwi"}
+diff1=set1-set2
+diff2=set2-set1
+print("set1-set2=",diff1)
+print("set2-set1=",diff2)
