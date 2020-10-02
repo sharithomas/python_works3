33c022ac880380e62a7a2f7f9c0771f73f7b03c6
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:03:37 +0530

    Update array_float.py

--- numpy_programs/array_float.py
+++ numpy_programs/array_float.py
@@ -4,3 +4,10 @@ import numpy as np
 data=[1,2,3,4,5,6,7,8]
 array=np.array(data,dtype="float")
 print("original array:",array)
+
+#another method
+import numpy as np
+array=np.arange(1,10)
+print("original array",array)
+array_float=np.asfarray(array)
+print("array of float type",array_float)
