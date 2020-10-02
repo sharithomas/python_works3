b476c7aebcb03d2f1c3fa14536f93701621fe5ef
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:07:26 +0530

    Create empty_full_array.py

---
+++ numpy_programs/empty_full_array.py
@@ -0,0 +1,7 @@
+# Write a NumPy program to create an empty and a full array.
+
+import numpy as np
+array_empty=np.empty((4,4))
+print("empty array\n",array_empty)
+array_full=np.full((4,4),6)
+print("full array\n",array_full)
