d53841f6f48178bd2ef6711ac9782e96ef0bcba9
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:11:41 +0530

    Create uniq_elem_array.py

---
+++ numpy_programs/uniq_elem_array.py
@@ -0,0 +1,7 @@
+#  Write a NumPy program to get the unique elements of an array.
+
+import numpy as np
+array=np.array([1,2,2,3,4,4,5,6,6])
+print("original array\n",array)
+print("unique elements of array\n")
+print(np.unique(array))
