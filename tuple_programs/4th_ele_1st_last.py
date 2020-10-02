44c77cda048067cf33293e38aa378ff38563f874
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 12:02:52 +0530

    Create 4th_ele_1st_last.py

---
+++ tuple_programs/4th_ele_1st_last.py
@@ -0,0 +1,6 @@
+# Write a Python program to get the 4th element and 4th element from last of a tuple
+
+tuple1=tuple(range(0,10))
+print("given tuple is",tuple1)
+print("4th element",tuple1[3])
+print("4th element from last",tuple1[-4])
