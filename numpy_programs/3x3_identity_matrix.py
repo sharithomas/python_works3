29e05384fee45c0d9985b4021043ae60eb4f6d38
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:15:37 +0530

    Create 3x3_identity_matrix.py

---
+++ numpy_programs/3x3_identity_matrix.py
@@ -0,0 +1,7 @@
+# Write a NumPy program to create a 3x3 identity matrix
+
+import numpy as np
+array1=np.diag([1,1,1])
+print("3x3 identity matrix\n",array1)
+array2=np.identity(3)
+print("3x3 identity matrix\n ",array2)
