fe4411e548218fee8988538ed697d525b0070dec
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:59:56 +0530

    Create tuple_string.py

---
+++ tuple_programs/tuple_string.py
@@ -0,0 +1,5 @@
+#6. Write a Python program to convert a tuple to a string
+
+tuple1='p','y','t','h','o','n'
+string1=''.join(tuple1) # convert tuple into string by joining each character of tuple 
+print(string1)
