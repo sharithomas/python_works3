cb6d7679b9029035da1a6df05f2590094b34b7d4
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:14:58 +0530

    Create array_even.py

---
+++ numpy_programs/array_even.py
@@ -0,0 +1,5 @@
+# Write a NumPy program to create an array of all the even integers from 30 to 70
+
+import numpy as np
+array=np.arange(30,71,2)
+print("array with all even numbers from 30 to 70=",array)
