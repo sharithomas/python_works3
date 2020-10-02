5287eff548b613c3d154c3b09ca7b9c178dc3351
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:47:38 +0530

    Create matrix_10x10.py

---
+++ matrix_10x10.py
@@ -0,0 +1,6 @@
+# Write a NumPy program to create a 10x10 matrix, in which the elements on the borders will be equal to 1, and inside 0
+
+import numpy as np
+array=np.ones((10,10))
+array[1:9,1:9]=0
+print("10x10 matrix=\n",array)
