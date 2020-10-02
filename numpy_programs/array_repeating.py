175a4853ef9ee192e01b34840513f063e933b07b
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:15:20 +0530

    Create array_repeating.py

---
+++ numpy_programs/array_repeating.py
@@ -0,0 +1,6 @@
+#  Write a NumPy program to construct an array by repeating
+
+import numpy as np
+array1=np.array([0,1,2,3])
+print("array1:\n",array1)
+print("reapeating 2 times\n",np.tile(array1,2))
