c9e983bd32dcd398c65a456c7a8e9187ae756101
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:00:36 +0530

    Create list_array.py

---
+++ numpy_programs/list_array.py
@@ -0,0 +1,7 @@
+# Write a NumPy program to convert a list of numeric value into a one-dimensional NumPy array
+
+import numpy as np
+list1=[12.23, 13.32, 100, 36.32]
+print("list1\n",list1)
+array=np.array(list1)
+print("numpy array\n",array)
