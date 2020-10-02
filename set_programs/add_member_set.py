bad5206ac8147cceb261387b103304b7f3efc8aa
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:45:15 +0530

    Create add_member_set.py

---
+++ set_programs/add_member_set.py
@@ -0,0 +1,7 @@
+# Write a Python program to add member(s) in a set
+
+set1= set((1,2,3,4,5,6,7,8))
+set1.add(22) # add single items
+print(set1)
+set1.update((100,200)) # add multiple items
+print(set1)
