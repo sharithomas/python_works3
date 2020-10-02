5a5723808cb1918f8b74bb27e975682ef0a4ef97
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:41:13 +0530

    Create array_random_15.py

---
+++ numpy_programs/array_random_15.py
@@ -0,0 +1,5 @@
+# Write a NumPy program to generate an array of 15 random numbers from a standard normal distribution
+
+import numpy as np
+rand_nums=np.random.normal(0,1,15)
+print("15 random numbers from standard normal distribution :",rand_nums)
