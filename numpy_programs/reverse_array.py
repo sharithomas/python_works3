7426812c66819962b79aeb07029adc0fc79bee13
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 17:02:25 +0530

    Create reverse_array.py

---
+++ numpy_programs/reverse_array.py
@@ -0,0 +1,6 @@
+#  Write a NumPy program to reverse an array (first element becomes last)
+
+import numpy as np
+array=np.arange(12,38)
+print("original array:",array)
+print("reversed array:",array[::-1])
