ca7e8685f0a74fd8b4283c84b2f5282e27cb4965
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 11:47:23 +0530

    Create intersection_sets.py

---
+++ set_programs/intersection_sets.py
@@ -0,0 +1,6 @@
+#  Write a Python program to create an intersection of sets
+
+my_set={"apple","orange","banana","plum"}
+my_set1={"banana","plum","kiwi"}
+set_intersection=my_set.intersection(my_set1)
+print(set_intersection)
