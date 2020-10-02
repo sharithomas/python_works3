d58433c8a51ba89a3a504c37ac5273c39f890264
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:05:29 +0530

    Create multiply_2_vectors.py

---
+++ numpy_programs/multiply_2_vectors.py
@@ -0,0 +1,10 @@
+# Write a NumPy program to multiply the values of two given vectors
+
+import numpy as np
+x=np.array([1,2,3,4])
+print("1st array")
+print(x)
+y=np.random.randint(1,11,4)
+print(y)
+print("multiplied value of vectors")
+print(x*y)
