434522e91bed0bdefcc31985c81562ff82771186
sharithomas <61357572+sharithomas@users.noreply.github.com>
2020-10-02 16:04:13 +0530

    Create count_row_col_matrix.py

---
+++ numpy_programs/count_row_col_matrix.py
@@ -0,0 +1,13 @@
+# Write a NumPy program to find the number of rows and columns of a given matrix
+
+import numpy as np
+x=np.arange(0,20).reshape(4,5)
+print("array is")
+print(x)
+print("no. of rows and columns")
+print(x.shape)
+
+#10. Write a NumPy program to create a 3x4 matrix filled with values from 10 to 21
+import numpy as np
+array=np.arange(10,22).reshape(3,4)
+print(array)
